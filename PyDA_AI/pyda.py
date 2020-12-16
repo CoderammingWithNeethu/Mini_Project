6e5d6898141a18ac7834602705df0dd0ea25bd53
Neethu Nath <neethu1027@gmail.com>
2020-12-17 03:50:11 +0530

    get details from wikipedia or wolfram or both and use text to speech using pttsx3 module with exception handling

--- PyDA_AI/pyda.py
+++ PyDA_AI/pyda.py
@@ -8,31 +8,55 @@ python code to access Wolframalpha
 https://account.wolfram.com/login/oauth2/sign-in
 https://developer.wolframalpha.com/portal/myapps/
 '''
-'''
-import wolframalpha
-client = wolframalpha.Client("AHP64J-L6XUYL5HUR")#AppID Deactived 
-res = client.query('how many continents in world')
-print(next(res.results).text)
-'''
+
 import PySimpleGUI as sg
+import wolframalpha
+import wikipedia
+import pyttsx3
+
+
+client = wolframalpha.Client("AHP64J-L6XUYL5HUR") #AppID Deactived Hence no peeking! :P
 
 sg.theme('DarkPurple')
 # Define the window's contents
-layout = [  [sg.Text("What's your name?")],     # Part 2 - The Layout
+layout = [  [sg.Text("Enter a command : ")],     # Each list is a horizontal reference in UI
             [sg.Input()],
-            [sg.Button('Ok')] ]
+            [sg.Button('Ok'),sg.Button('Cancel')] ] #to show buttons on single level horizontally
 
 # Create the window
-window = sg.Window('Window Title', layout)      # Part 3 - Window Defintion
-
+window = sg.Window('PyDa', layout)      # Window Defintion
 
 # Display and interact with the Window
 while True :
-    event, values = window.read()                   # Part 4 - Event loop or Window.read call
+    event, values = window.read()    # Event loop or Window.read call
     if event in (None, 'Cancel'):
         break
-    # Do something with the information gathered
-    print('YOUR TEXT : ', values[0])
+    res = client.query(values[0])
 
+    print('YOUR TEXT : ', values[0]) 
+    engine = pyttsx3.init()
+    try : 
+        wiki_res = wikipedia.summary(values[0]) #faster and detailed response 
+        wolfram_res = next(res.results).text
+        #text to speech
+        engine.say(wiki_res)
+        engine.say(wolfram_res)    
+        sg.PopupNonBlocking('WOLFRAM RESULT : '+wiki_res +'\n\nWIKIPEDIA RESULT :'+wolfram_res) #PopupNonBlocking allow to read text and show parallely 
+    except wikipedia.exceptions.DisambiguationError:
+        wolfram_res = next(res.results).text
+        engine.say(wolfram_res)    
+        sg.PopupNonBlocking('WIKIPEDIA RESULT :'+wolfram_res)
+    except wikipedia.exceptions.PageError:
+        wolfram_res = next(res.results).text
+        engine.say(wolfram_res)    
+        sg.PopupNonBlocking('WIKIPEDIA RESULT :'+wolfram_res)
+    except:
+        wiki_res = wikipedia.summary(values[0])
+        engine.say(wiki_res)    
+        sg.PopupNonBlocking('WIKIPEDIA RESULT :'+wiki_res)
+
+    #sg.Popup('WOLFRAM RESULT : '+wiki_res +'\n\nWIKIPEDIA RESULT :'+wolfram_res)
+    engine.runAndWait()
+    
 # Finish up by removing from the screen
 window.close()
